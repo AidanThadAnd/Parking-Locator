c68d22bead65a05239ddefb75fd840f31afd191a
austin.simmons <austin.simmons@ucalgary.ca>
2024-02-17 15:48:35 -0700

    convert coord to km method added

--- backend/backend.py
+++ backend/backend.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Perform tasks with CSV data here
-    pass
+    return "lol"
 
 def read_data_from_csv(filename):
     # Logic to read data from CSV
+
     pass
 
+def convert_coord_km(lat, long):
+    return lat*111.2 + long*111.3
 
 if __name__ == '__main__':
     app.run()
