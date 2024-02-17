2b68a053d7441c2116894eaa4493884cc1d8b198
austin.simmons <austin.simmons@ucalgary.ca>
2024-02-17 16:24:36 -0700

    read_data_from_csv, convert_coord_km

--- backend/backend.py
+++ backend/backend.py
@@ -1,19 +1,26 @@
 from flask import Flask
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     # Perform tasks with CSV data here
-    return "lol"
+    onsDF = read_data_from_csv('backend/datasets/On-Street_Parking_Zones_20240217.csv')
+    resDF = read_data_from_csv('backend/datasets/On-Street_Residential_Parking_Zones_20240217.csv')
+    return str(resDF)
 
 def read_data_from_csv(filename):
     # Logic to read data from CSV
+    dataFrame = pd.read_csv(filename)
+    return dataFrame
 
-    pass
-
+# converts lat and long into km
 def convert_coord_km(lat, long):
-    return lat*111.2 + long*111.3
+    return abs(lat*111.2) + abs(long*111.3)
+
+
+
 
 if __name__ == '__main__':
     app.run()
