64d9bf7f594267bcd76c634d2c6f697273f65bab
Jerrit <Jortmcchez@gmail.com>
2024-02-17 23:01:40 -0700

    yea git lol

---
+++ backend/ihategit.py
@@ -0,0 +1,102 @@
+from flask import Flask
+import pandas as pd
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Perform tasks with CSV data here
+    onsDF = read_data_from_csv('backend/datasets/On-Street_Parking_Zones_20240217.csv')
+    resDF = read_data_from_csv('backend/datasets/On-Street_Residential_Parking_Zones_20240217.csv')
+    userCoords = get_address_coords('1400 12 Ave SW, Calgary')
+    res_filter_useless_restrictions(resDF)
+    ons_filter_non_parking_zones(onsDF)
+    return onsDF
+
+def read_data_from_csv(filename):
+    # Logic to read data from CSV
+    dataFrame = pd.read_csv(filename)
+    return dataFrame
+
+
+def important_method(desiredLength, desiredDistance):
+    onsDF = read_data_from_csv('backend/datasets/On-Street_Parking_Zones_20240217.csv')
+    resDF = read_data_from_csv('backend/datasets/On-Street_Residential_Parking_Zones_20240217.csv')
+
+    res_filter_useless_restrictions(resDF)
+    ons_filter_non_parking_zones(onsDF)
+
+    res_filter_parking_restrictions(resDF, desiredLength)
+    return
+
+# filter useless restrictions
+def res_filter_useless_restrictions(dataFrame):
+    res_filter_parking_restrictions(dataFrame, 'Payment Required')
+    res_filter_parking_restrictions(dataFrame, 'Special Permit')
+    res_filter_parking_restrictions(dataFrame, 'Handicap Permit Required')
+    res_filter_parking_restrictions(dataFrame, 'EOC Permit')
+    return dataFrame
+
+# filters for users desired parking length
+def ons_filter_parking_times(dataFrame, desiredRestriction):
+    for x in dataFrame.index:
+        if dataFrame.loc[x, "MAX_TIME"] < desiredRestriction:
+            dataFrame.drop(x, inplace = True)
+    return dataFrame
+
+# converts lat and long into km
+def convert_coord_km(lat, long):
+    return abs(lat*111.2) + abs(long*111.3)
+
+# Filters the dataFrame by a desired parking restriction in the csv
+def res_filter_parking_restrictions(dataFrame, desiredRestriction):
+    if isinstance(desiredRestriction, int):
+        for x in dataFrame.index:
+            if dataFrame.loc[x,"PARKING_RESTRICTION"] != 'NONE' and int(dataFrame.loc[x,"PARKING_RESTRICTION"]) < desiredRestriction:
+                dataFrame.drop(x, inplace = True)
+    else:
+        for x in dataFrame.index:
+            if dataFrame.loc[x,"PARKING_RESTRICTION"] == desiredRestriction:
+                dataFrame.drop(x, inplace = True)
+    return dataFrame
+
+#filters non-parking zones
+def ons_filter_non_parking_zones(dataFrame):
+    for x in dataFrame.index:
+        if dataFrame.loc[x, "ZONE_TYPE"] != 'Parking Zone':
+            dataFrame.drop(x, inplace = True)
+    return dataFrame
+
+# This method uses the canadian governments free geolocation service to extract matching coordinates based on 
+# provided address returns list of floats
+def get_address_coords(address):
+    # 1400 12 Ave SW, Calgary, AB T3C 0P7, 51.043345859995114, -114.0940433180538
+    addressDF = requests.get('https://geogratis.gc.ca/services/geolocation/en/locate?q=' + address)
+    userAddressList = addressDF.json()
+
+    for x in userAddressList[1].values():
+        if type(x) is dict:
+            return x.get('coordinates')
+
+# Parses the dataframe for coords and returns a list of lists containing floats for further analysis
+# Returns list of list of floats
+def get_float_coords(dataFrame):
+    coordsList = []
+    for string in dataFrame.loc[:, "line"]:
+        getvals = []
+        for val in string:
+            if val.isdigit() or val.isspace() or val == '.':
+                getvals.append(val)
+        result = "".join(getvals)
+        result = result.strip()
+        list2 = result.split(" ")
+        listnum = []
+        for s in list2:
+            z = float(s)
+            listnum.append(z)
+        coordsList.append(listnum)
+    return coordsList
+
+if __name__ == '__main__':
+    app.run()
